214f062990ae12d5a664ded21656e8451589aa60
Bobby Argenbright <arg3nbright@gmail.com>
2016-01-28 14:48:08 -0800

    added travis config

--- cirtkit.py
+++ cirtkit.py
@@ -2,21 +2,10 @@
 # This file is part of Cirtkit - https://github.com/cirtkit-framework/cirtkit
 
 import argparse
-from sys import argv, executable
-from os import path, symlink, getcwd
 
 from lib.core.ui import console
 from lib.core.investigation import __project__
 from lib.core.database import Database
-
-if argv[0][-3:] == '.py':
-    # create symlink
-    try:
-        src = getcwd() + '/' + argv[0]
-        dst = executable
-        print dst
-    except:
-        pass
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--investigation', help='Specify a new or existing investigation', action='store', required=False)
